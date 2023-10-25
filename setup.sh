a7517a430a1ea55e3ada2cdb4fbd3abeb711d3b8
Amir <148009247+v3meduu@users.noreply.github.com>
2023-10-25 16:59:16 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,15 +1,34 @@
 #!/bin/bash
 
-DISCORD_WEBHOOK_URL="YOUR_DISCORD_WEBHOOK_URL"
-HOST_NAME="PteroShield"
 CONFIG_FILE="/root/pteroshield/config.yml"
 
-configure_discord_webhook() {
+prompt_for_configuration() {
   read -p "Enter your Discord webhook URL: " DISCORD_WEBHOOK_URL
   read -p "Enter your host name: " HOST_NAME
+  read -p "Enter the storage limit in GB (e.g., 100G) to prevent diskfills: " STORAGE_LIMIT
+  read -p "Enter the network limit for each server in mbps (e.g., 100mbit): " NETWORK_LIMIT
+  read -p "Do you want to allocate a swap file? (Y/N): " SWAP_FILE
+  read -p "Do you want to block default ports? (Y/N): " BLOCK_PORTS
+  if [ "$BLOCK_PORTS" == "Y" ] || [ "$BLOCK_PORTS" == "y" ]; then
+    read -p "Enter the ports you want to block (e.g., 465 25 995): " -a BLOCKED_PORTS
+  fi
+  read -p "What ports are you going to allocate for Pterodactyl Wings game ports (e.g., 5000-6000)? " PTERODACTYL_WINGS_PORTS
+  read -p "Do you want to run the Pterodactyl Wings installation now? (Y/N): " PTERODACTYL_WINGS_INSTALL
+
   echo "DISCORD_WEBHOOK_URL=\"$DISCORD_WEBHOOK_URL\"" >> "$CONFIG_FILE"
   echo "HOST_NAME=\"$HOST_NAME\"" >> "$CONFIG_FILE"
-  echo "Discord webhook and host name configured."
+  echo "STORAGE_LIMIT=\"$STORAGE_LIMIT\"" >> "$CONFIG_FILE"
+  echo "NETWORK_LIMIT=\"$NETWORK_LIMIT\"" >> "$CONFIG_FILE"
+  echo "SWAP_FILE=\"$SWAP_FILE\"" >> "$CONFIG_FILE"
+  if [ "$BLOCK_PORTS" == "Y" ] || [ "$BLOCK_PORTS" == "y" ]; then
+    echo "BLOCK_PORTS=\"$BLOCK_PORTS\"" >> "$CONFIG_FILE"
+    for P in "${BLOCKED_PORTS[@]}"; do
+      echo "BLOCKED_PORTS+=($P)" >> "$CONFIG_FILE"
+    done
+  fi
+  echo "PTERODACTYL_WINGS_PORTS=\"$PTERODACTYL_WINGS_PORTS\"" >> "$CONFIG_FILE"
+  echo "PTERODACTYL_WINGS_INSTALL=\"$PTERODACTYL_WINGS_INSTALL\"" >> "$CONFIG_FILE"
+  echo "Configuration completed and saved to $CONFIG_FILE."
 }
 
 create_default_config() {
@@ -18,17 +37,15 @@ create_default_config() {
   if [ ! -f "$CONFIG_FILE" ]; then
     echo "Creating a default configuration file..."
     cat <<EOF >"$CONFIG_FILE"
-DISCORD_WEBHOOK_URL="$DISCORD_WEBHOOK_URL"
-HOST_NAME="$HOST_NAME"
-SL=100G
-BL=100mbit
-NL=100mbit
-NWDL=100mbit
-WP="5000-6000"
-IW="N"
-CS="N"
-SSG=2
-BP="Y"
+DISCORD_WEBHOOK_URL=""
+HOST_NAME=""
+STORAGE_LIMIT=""
+NETWORK_LIMIT=""
+SWAP_FILE=""
+BLOCK_PORTS=""
+BLOCKED_PORTS=()
+PTERODACTYL_WINGS_PORTS=""
+PTERODACTYL_WINGS_INSTALL=""
 EOF
     echo "Default configuration file created at $CONFIG_FILE"
   fi
@@ -46,7 +63,7 @@ setup_log_directory() {
 
 create_swap_file() {
   load_configuration
-  if [ "$CS" == "Y" ] || [ "$CS" == "y" ]; then
+  if [ "$SWAP_FILE" == "Y" ] || [ "$SWAP_FILE" == "y" ]; then
     read -p "How many GB swap file do you want to allocate? " SSG
     if [[ "$SSG" =~ ^[0-9]+$ ]]; then
       SSGB=$((SSG * 1024 * 1024 * 1024))
@@ -66,8 +83,7 @@ create_swap_file() {
 block_ports_and_install_iptables() {
   load_configuration
 
-  if [ "$BP" == "Y" ] || [ "$BP" == "y" ]; then
-    BLOCKED_PORTS=(465 25 995 143 993 587 5222 5269 5443)
+  if [ "$BLOCK_PORTS" == "Y" ] || [ "$BLOCK_PORTS" == "y" ]; then
     for P in "${BLOCKED_PORTS[@]}"; do
       sudo iptables -A INPUT -p tcp --dport "$P" -j DROP
       sudo iptables -A INPUT -p udp --dport "$P" -j DROP
@@ -84,8 +100,8 @@ block_ports_and_install_iptables() {
 
 update_docker_settings() {
   load_configuration
-  SL="$SL"
-  BL="$BL"
+  SL="$STORAGE_LIMIT"
+  BL="$NETWORK_LIMIT"
   for CU in $(docker ps -q); do
     docker update --storage-opt size="$SL" $CU
     docker exec $CU tc qdisc add dev eth0 root tbf rate "$BL" burst 10kbit latency 50ms
@@ -94,46 +110,30 @@ update_docker_settings() {
 
 allow_pterodactyl_wings() {
   load_configuration
-  read -p "What ports are you going to allocate for Pterodactyl Wings game ports (e.g., 5000-6000)? " WP
-
-  if [[ "$WP" =~ ^[0-9]+-[0-9]+$ ]]; then
-    IFS='-' read -ra PR <<< "$WP"
-    SP="${PR[0]}"
-    EP="${PR[1]}"
-    sudo ufw allow "$SP":"$EP/tcp"
-    sudo ufw allow "$SP":"$EP/udp"
-    echo "Ports $SP to $EP for Pterodactyl Wings have been allowed (TCP and UDP)."
-  else
-    echo "Invalid input. Please provide a valid range in the format of 'start-end' (e.g., 5000-5999)."
-  fi
+  if [ -n "$PTERODACTYL_WINGS_PORTS" ]; then
+    if [[ "$PTERODACTYL_WINGS_PORTS" =~ ^[0-9]+-[0-9]+$ ]]; then
+      IFS='-' read -ra PR <<< "$PTERODACTYL_WINGS_PORTS"
+      SP="${PR[0]}"
+      EP="${PR[1]}"
+      sudo ufw allow "$SP":"$EP/tcp"
+      sudo ufw allow "$SP":"$EP/udp"
+      echo "Ports $SP to $EP for Pterodactyl Wings have been allowed (TCP and UDP)."
+    else
+      echo "Invalid input for Pterodactyl Wings ports. Please provide a valid range in the format of 'start-end' (e.g., 5000-5999)."
+    fi
+  else {
+    echo "No Pterodactyl Wings ports specified."
+  }
 }
 
 install_pterodactyl_wings() {
   load_configuration
-  read -p "Do you want to run the Pterodactyl Wings installation now? (Y/N): " IW
-
-  if [ "$IW" == "Y" ] || [ "$IW" == "y" ]; then
+  if [ "$PTERODACTYL_WINGS_INSTALL" == "Y" ] || [ "$PTERODACTYL_WINGS_INSTALL" == "y" ]; then
     echo "Installing Pterodactyl Wings..." | tee -a "$LF"
     bash <(curl -s https://pterodactyl-installer.se/) 2>&1 | tee -a "$LF"
     echo "Pterodactyl Wings installation completed." | tee -a "$LF"
   else
     echo "Pterodactyl Wings installation skipped. You can run it manually when ready." | tee -a "$LF"
-  fi
-}
-
-allow_pterodactyl_wings() {
-  load_configuration
-  read -p "What ports are you going to allocate for Pterodactyl Wings game ports (e.g., 5000-6000)? " WP
-
-  if [[ "$WP" =~ ^[0-9]+-[0-9]+$ ]]; then
-    IFS='-' read -ra PR <<< "$WP"
-    SP="${PR[0]}"
-    EP="${PR[1]}"
-    sudo ufw allow "$SP":"$EP/tcp"
-    sudo ufw allow "$SP":"$EP/udp"
-    echo "Ports $SP to $EP for Pterodactyl Wings have been allowed (TCP and UDP)."
-  else
-    echo "Invalid input. Please provide a valid range in the format of 'start-end' (e.g., 5000-5999)."
   fi
 }
 
@@ -157,7 +157,8 @@ monitor_container_cpu_usage() {
   done
 }
 
-configure_discord_webhook
+# Main script
+prompt_for_configuration
 create_default_config
 setup_log_directory
 create_swap_file
@@ -181,6 +182,5 @@ install_pterodactyl_wings() {
   fi
 }
 
-# Display completion messages
 echo "Script Version: $SCRIPT_VERSION installed!" | tee -a "$LOG_FILE"
 echo "PteroShield execution completed." | tee -a "$LOG_FILE"
